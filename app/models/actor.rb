a6038672502d799b94fb99d487cca774179bb5a4
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-04-14 10:08:17 -0400

    commit tree b08e5f677b53b298fbacd22a2c7c6fc47c012ed3

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    #returns the first and last name of an actor
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #lists all the characters of an actor alongside the show that the character is in
+    #  ["#{character.name} - #{show.name}"]
+    self.characters.map {|c| "#{c.name} - #{c.show.name}"}
+  end
+
+
   
 end
